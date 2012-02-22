efa41d5075aa98d4ac3e82520a3402db20ef42b8
Bibek Shrestha <bibek@yipl.com.np>
2012-02-22 14:08:55 +0100

    Default date/time added

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,4 @@
 <?php
+date_default_timezone_set('Asia/Kathmandu');
 if (!defined('APP_PATH')) define('APP_PATH', realpath(dirname(__FILE__)));
 if (!defined('FILE_PATH')) define('FILE_PATH', realpath(APP_PATH . '/../files'));
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Kathmandu');
 if (!defined('TEST_PATH')) define('TEST_PATH', dirname(__FILE__));
 if (!defined('APP_PATH')) define('APP_PATH', realpath(TEST_PATH . '/../src'));
 if (!defined('FILE_PATH')) define('FILE_PATH', realpath(TEST_PATH . '/../files'));
