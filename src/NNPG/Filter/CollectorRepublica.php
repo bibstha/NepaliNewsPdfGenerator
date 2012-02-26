de3a07c49dfdd3d69645b90c6c5db815567f127f
Bibek Shrestha <bibek@yipl.com.np>
2012-02-26 04:44:36 +0100

    Update

--- src/NNPG/Filter/CollectorRepublica.php
+++ src/NNPG/Filter/CollectorRepublica.php
@@ -1,84 +1,75 @@
 <?php
-require_once("NNPG/Utils/FileProxy.php");
-require_once("NNPG/Utils/ImageToPDF.php");
-
-class NNPG_Filter_CollectorRepublica
+class NNPG_Filter_CollectorRepublica extends NNPG_Filter_CollectorAbstract
 {
-    protected $_params;
     protected $_name = 'Republica';
-    
-    public function __construct($params)
-    {
-        $this->_checkParams($params);
-        $this->_filterParams($params);
-        $this->_params = $params;
-    }
-    
-    public function process()
-    {
-        return array(
-            'inPaths' => $this->_downloadAndGenerateFileList(),
-            'outPath' => FILE_PATH . '/combined/' . $this->_name . '/' . 
-                $this->_getDateForFileName() . '.pdf',
-        );
-    }
-    
-    /**
-     * Check if mandatory parameters are set or not, does not modify params variable
-     * 
-     * @param array $params
-     * @throws Exception
-     */
-    protected function _checkParams($params)
-    {
-        // required : date (any format that is recognized by strtotime)
-        if (!isset($params['date'])) throw new Exception("Parameter 'date' not found");
-    }
-    
-    /**
-     * Modification in $params can only occur in filterParams
-     * 
-     * @param array $params
-     */
-    protected function _filterParams(&$params)
-    {
-        $params['date'] = strtotime($params['date']);
-        if (!isset($params['pageCount'])) $params['pageCount'] = 16;
-    }
-    
-    protected function _downloadAndGenerateFileList()
-    {
-        $imgToPdf = new NNPG_Util_ImageToPDF();
-        
-        $dateInFilename = $this->_getDateForFileName();
-        $numOfPages = $this->_params['pageCount'];
-
-        $fileList = array();
-        for ($i = 1; $i <= $numOfPages; $i++) {
-            $dateForFileName = $this->_getDateForFileName();
-            $imageFilePath = 'single/' . $this->_name . "/$dateForFileName/p$i.jpg";
-            $url = $this->_getUrl($i);
-            $fileProxy = new NNPG_Utils_FileProxy();
-            $fileProxy->setName($imageFilePath);
-            $fileProxy->setUrl($url);
-            $isSuccess = $fileProxy->download();
-            $imagePath = $fileProxy->getPath();
-            $pdfPath = FILE_PATH . '/single/' . $this->_name . "/$dateForFileName/p$i.pdf";
-            $imgToPdf->convert($imagePath, $pdfPath);
-            $fileList[] = $pdfPath;
-        }
-        return $fileList;
-    }
+    protected $_pages = array();
     
     protected function _getUrl($pageNumber)
     {
-        $date = strtolower( date('Y_M_d', $this->_params['date']) );
-        $urlTpl = 'http://e.myrepublica.com/images/flippingbook/%s/republica/rp_zoom_%02d.jpg';
-        return sprintf($urlTpl, $date, $pageNumber);
+        
+    }
+
+    protected function _checkAndFilterParams($params)
+    {
+        if (!isset($params['date'])) throw new Exception("Parameter 'date' should be set");
+    }
+
+    protected function _processInput()
+    {
+        // Do initialization by crawling republica website
+        $this->_crawlAndExtractRepublica();
+        
+        $imgToPdf = new NNPG_Util_ImageToPDF();
+        
+        $dateInFilename = $this->_getDateForFileName();
+        $numOfPages = $this->_getPageCount();
+        
+        $fileList = array();
+        $filePathTpl = 'single/%1$s/%2$s/%3$s';
+        for ($i = 1; $i <= $numOfPages; $i++) {
+            $filePath = sprintf($filePathTpl, $this->_name, $dateInFilename, "p$i.jpg");
+            print $filePath . "\n";
+            $url = $this->_pages[$i-1];
+            $fileProxy = new NNPG_Utils_FileProxy();
+            $fileProxy->setName($filePath);
+            $fileProxy->setUrl($url);
+            $isSuccess = $fileProxy->download();
+            $imagePath = $fileProxy->getPath();
+            $pdfPath = FILE_PATH . '/single/' . $this->_name . "/$dateInFilename/p$i.pdf";
+            $imgToPdf->convert($imagePath, $pdfPath);
+            $fileList[] = $pdfPath;
+        }
+        
+        return $fileList;
     }
     
-    protected function _getDateForFileName()
+    protected function _getDate()
     {
-        return date('Y-m-d', $this->_params['date']);
+        // Not used
+        // return date('jnY', strtotime($this->_params['date']) );
+    }
+
+    protected function _getPageCount()
+    {
+        return count($this->_pages);
+    }
+    
+    protected function _crawlAndExtractRepublica()
+    {
+        // page with link to individual prints
+        $xml = file_get_contents('http://e.myrepublica.com');
+        preg_match_all('#href="/component/flippingbook/book/(.*)"#U', $xml, $matches);
+        
+        // Change latestUrl to get date-wise-url
+        $latestUrl = 'http://e.myrepublica.com/component/flippingbook/book/' . $matches[1][0];
+        
+        // page with one print
+        $xml = file_get_contents($latestUrl);
+        preg_match("#flippingBook[0-9]+.enlargedImages = \[(.*)\]#sU", $xml, $matches);
+        $pages = $matches[1];
+        preg_match_all("#/images/.*\.jpg#U", $pages, $matches);
+        foreach ($matches[0] as $key => $match) {
+            $this->_pages[$key] = 'http://e.myrepublica.com' . $match;
+        }
     }
 }
