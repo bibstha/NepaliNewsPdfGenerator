da2c1ebbd549fdaa49484fc934890a322437f967
Bibek Shrestha <bibek@yipl.com.np>
2012-05-10 10:37:25 +0200

    Command line tool Download.php created

---
+++ example/Download.php
@@ -0,0 +1,41 @@
+<?php
+require_once('../src/bootstrap.php');
+$papers = array(
+	'ktmpost' => 'KTMPost',
+	'kantipur' => 'Kantipur',
+	'republica' => 'Republica',
+	'nagarik' => 'Nagarik',
+	'tht' => 'THT',
+);
+
+$paramsKeys = array_merge( array("date::"), array_keys($papers));
+$paramsVals = getopt(null, $paramsKeys);
+
+$date = date('Y-m-d');
+if (isset($paramsVals['date']) && $paramsVals['date'] ) {
+	$date = $paramsVals['date'];
+}
+
+$papersToDownload = array_intersect_key($papers, $paramsVals);
+if (empty($papersToDownload)) {
+	print <<<EOF
+No papers specified for download. Please enter atleast one paper from the following list.
+
+Usage: php Download.php [OPTIONS]
+
+OPTIONS:
+--ktmpost   for Kathmandu post
+--kantipur  for Kantipur
+--republica for Republica
+--nagarik   for Nagarik
+--tht       for TheHimalayanTimes
+
+
+
+EOF;
+}
+
+foreach ($papersToDownload as $paper) {
+	$obj = new NNPG_Generator();
+	$obj->generate($paper, $date);
+}
